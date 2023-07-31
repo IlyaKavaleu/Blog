f727f588dd459c28bfef35e97de2f6ea7adc213d
ilia <kavaleuilia@gmail.com>
2023-07-31 20:47:59 +0200

    Add fixtures and upgrade old tests and add new tests

--- events/tests.py
+++ events/tests.py
@@ -1,14 +1,31 @@
 from http import HTTPStatus
-
+from django.http import HttpResponse
 from django.test import TestCase
 from django.urls import reverse
+from .models import Event
 
 
 class EventTestCase(TestCase):
     """Test for homepage"""
+    def setUp(self):
+        self.path = reverse('events:home')
+        self.response = self.client.get(self.path)
+
     def test_view(self):
+        self.assertEquals(self.response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(self.response, 'events/home.html')
+
+    def test_view_events_page(self):
         path = reverse('events:home')
         response = self.client.get(path)
 
         self.assertEquals(response.status_code, HTTPStatus.OK)
         self.assertTemplateUsed(response, 'events/home.html')
+
+    def test_post(self):
+        events = Event.objects.all()
+        path = reverse('events:home')
+
+        response_data = [event for event in events]
+        response = HttpResponse(response_data, content_type='text/type')
+        self.assertEquals(list(response_data), list(events))
--- posts/tests.py
+++ posts/tests.py
@@ -1,4 +1,6 @@
 from http import HTTPStatus
+from django.http import HttpResponse
+
 from django.test import TestCase
 from django.urls import reverse
 from .models import Post
@@ -6,11 +8,21 @@ from .models import Post
 
 class PostsTestCase(TestCase):
     """Test for show_post page"""
-    def test_view(self):
+    fixtures = ['fixtures/posts.json']
+
+    def test_view_posts_page(self):
+        """Checking status code for page show_page and
+         check that we are using the correct template"""
         path = reverse('posts:show_post')
         response = self.client.get(path)
 
         self.assertEquals(response.status_code, HTTPStatus.OK)
         self.assertTemplateUsed(response, 'posts/show_post.html')
 
-
+    def test_posts_db(self):
+        """Сhecking that the amount of data on the server and in the database is the same"""
+        posts = Post.objects.all()
+        path = reverse('posts:show_post')
+        response_data = [post for post in posts]
+        response = HttpResponse(response_data, content_type='text/type')
+        self.assertEquals(list(response_data), list(posts))
