6d83e04ea603e0232302c7d80995bf803898f99c
ilia <kavaleuilia@gmail.com>
2023-07-31 15:07:59 +0200

    Update base.html

--- posts/views.py
+++ posts/views.py
@@ -7,7 +7,9 @@ def show_post(request):
     context = {'posts': posts}
     return render(request, 'posts/show_post.html', context)
 
+
 def one_post(request, post_id):
     post = Post.objects.get(id=post_id)
     context = {'post': post}
     return render(request, 'posts/one_post.html', context)
+
